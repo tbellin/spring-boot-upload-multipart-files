8cbf3c08af024298c2a6b4295ec5a216ef0428db
Tiziano <tizianobellin@yahoo.com>
2024-04-25 15:23:38 +0200

    insert param spring.folder in application.properties

--- src/main/java/com/bezkoder/spring/files/upload/service/FilesStorageServiceImpl.java
+++ src/main/java/com/bezkoder/spring/files/upload/service/FilesStorageServiceImpl.java
@@ -8,6 +8,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
@@ -17,68 +18,73 @@ import org.springframework.web.multipart.MultipartFile;
 @Service
 public class FilesStorageServiceImpl implements FilesStorageService {
 
-  private final Path root = Paths.get("uploads");
+	@Value("${spring.folder}")
+	private String folder;
 
-  @Override
-  public void init() {
-    try {
-      Files.createDirectories(root);
-    } catch (IOException e) {
-      throw new RuntimeException("Could not initialize folder for upload!");
-    }
-  }
+	private final Path root = Paths.get("uploads");
 
-  @Override
-  public void save(MultipartFile file) {
-    try {
-      Files.copy(file.getInputStream(), this.root.resolve(file.getOriginalFilename()));
-    } catch (Exception e) {
-      if (e instanceof FileAlreadyExistsException) {
-        throw new RuntimeException("A file of that name already exists.");
-      }
+	@Override
+	public void init() {
+		try {
+			Files.createDirectories(root);
+			Path folderPath = Paths.get(folder);
+			Files.createDirectories(folderPath);
+		} catch (IOException e) {
+			throw new RuntimeException("Could not initialize folder for upload!");
+		}
+	}
 
-      throw new RuntimeException(e.getMessage());
-    }
-  }
+	@Override
+	public void save(MultipartFile file) {
+		try {
+			Files.copy(file.getInputStream(), this.root.resolve(file.getOriginalFilename()));
+		} catch (Exception e) {
+			if (e instanceof FileAlreadyExistsException) {
+				throw new RuntimeException("A file of that name already exists.");
+			}
 
-  @Override
-  public Resource load(String filename) {
-    try {
-      Path file = root.resolve(filename);
-      Resource resource = new UrlResource(file.toUri());
+			throw new RuntimeException(e.getMessage());
+		}
+	}
 
-      if (resource.exists() || resource.isReadable()) {
-        return resource;
-      } else {
-        throw new RuntimeException("Could not read the file!");
-      }
-    } catch (MalformedURLException e) {
-      throw new RuntimeException("Error: " + e.getMessage());
-    }
-  }
+	@Override
+	public Resource load(String filename) {
+		try {
+			Path file = root.resolve(filename);
+			Resource resource = new UrlResource(file.toUri());
 
-  @Override
-  public boolean delete(String filename) {
-    try {
-      Path file = root.resolve(filename);
-      return Files.deleteIfExists(file);
-    } catch (IOException e) {
-      throw new RuntimeException("Error: " + e.getMessage());
-    }
-  }
+			if (resource.exists() || resource.isReadable()) {
+				return resource;
+			} else {
+				throw new RuntimeException("Could not read the file!");
+			}
+		} catch (MalformedURLException e) {
+			throw new RuntimeException("Error: " + e.getMessage());
+		}
+	}
 
-  @Override
-  public void deleteAll() {
-    FileSystemUtils.deleteRecursively(root.toFile());
-  }
+	@Override
+	public boolean delete(String filename) {
+		try {
+			Path file = root.resolve(filename);
+			return Files.deleteIfExists(file);
+		} catch (IOException e) {
+			throw new RuntimeException("Error: " + e.getMessage());
+		}
+	}
 
-  @Override
-  public Stream<Path> loadAll() {
-    try {
-      return Files.walk(this.root, 1).filter(path -> !path.equals(this.root)).map(this.root::relativize);
-    } catch (IOException e) {
-      throw new RuntimeException("Could not load the files!");
-    }
-  }
+	@Override
+	public void deleteAll() {
+		FileSystemUtils.deleteRecursively(root.toFile());
+	}
+
+	@Override
+	public Stream<Path> loadAll() {
+		try {
+			return Files.walk(this.root, 1).filter(path -> !path.equals(this.root)).map(this.root::relativize);
+		} catch (IOException e) {
+			throw new RuntimeException("Could not load the files!");
+		}
+	}
 
 }
